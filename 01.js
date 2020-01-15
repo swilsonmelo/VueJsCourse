918feb2eb374c93d6e961f5e7f70b7f03abd702a
swilsonmelo <swilsonmelo@gmail.com>
2020-01-15 08:15:27 -0500

    end of the first video introduction VueJS

--- 01.js
+++ 01.js
@@ -1,7 +1,12 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'Hola mundo con vue'
+        titulo: 'Hola mundo con vue',
+        frutas: [
+            {nombre:'pera', cantidad: 0},
+            {nombre:'Manzana', cantidad: 20},
+            {nombre:'platano', cantidad: 30}
+        ]
     }
 
 })
